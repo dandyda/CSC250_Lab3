cf2eb5a0f93d0c6f5ff7d80a13ccc07b5ef38fac
dandyda <46979665+dandyda@users.noreply.github.com>
2019-03-13 22:11:01 -0500

    Update myFunctionLib.c

--- myFunctionLib.c
+++ myFunctionLib.c
@@ -1,6 +1,10 @@
+//Dino Dan, CSC-250-SP19
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #include "myFunctionLib.h"
 #include "myGfxShapesLib.h"
 #include "gfx2.h"
@@ -9,9 +13,12 @@
 void sortVisualize (double* listOfDoubles, int COUNT)
 {
     int i;
+    
     for (i=0; i < COUNT; i++)  {
         printf("%lf\n", listOfDoubles[i]);
+       
     }
+    
 
 }//end printListOfDoubles
 
@@ -25,15 +32,17 @@ void sortNumbers(double* listOfDoubles, int COUNT)
     for (i=0; i < COUNT; i++)  {
         num = rand() / (double) RAND_MAX * 200.0;
         listOfDoubles[i] = num;
+        
     }
     
     for (i=0; i < COUNT; i++)  {
         
-        for (j=0; j < COUNT - 1; j++)  {
+        for (j=0; j < COUNT - 1; j++)  { 
             if (listOfDoubles[j] > listOfDoubles[j+1])  {
                 temp=listOfDoubles[j];
                 listOfDoubles[j] = listOfDoubles[j+1];
                 listOfDoubles[j+1]=temp;
+               
             } //end if
         }//end for    
     }//end for
