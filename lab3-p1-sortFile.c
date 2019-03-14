ef1760ae4c1356ab4c2a3f87288ac24beb137010
dandyda <46979665+dandyda@users.noreply.github.com>
2019-03-13 21:18:11 -0500

    Update lab3-p1-sortFile.c

--- lab3-p1-sortFile.c
+++ lab3-p1-sortFile.c
@@ -1,3 +1,4 @@
+
 /*****DINO DAN, CSC250-SP19
 ******LAB 3, SORT RANDOM NUMBER BAR GRAPH
 ******Major contribution by Noah Manter
@@ -5,53 +6,31 @@
 
 
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "myFunctionLib.h"
 #include "myGfxShapesLib.h"
 #include "gfx2.h"
 
 
 
-void printListOfDoubles (double* listOfDoubles, int COUNT)
-{
-    int i;
-    for (i=0; i < COUNT; i++)  {
-        printf("%lf\n", listOfDoubles[i]);
-    }
-
-}
-
 int main ()
 {
     int COUNT=20;
     double listOfDoubles[COUNT];
-    int i, j;
     int xSize=600, ySize=600;
-    int temp=0;
-    int num;
+    
     
     
     
     srand(time(NULL));
     
-    for (i=0; i < COUNT; i++)  {
-        num = rand() / (double) RAND_MAX * 200.0;
-        listOfDoubles[i] = num;
-    }
-    
-    for (i=0; i < COUNT; i++)  {
+    sortNumbers(listOfDoubles, COUNT);
         
-    for (j=0; j < COUNT - 1; j++)  {
-        if (listOfDoubles[j] > listOfDoubles[j+1])  {
-            temp=listOfDoubles[j];
-            listOfDoubles[j] = listOfDoubles[j+1];
-            listOfDoubles[j+1]=temp;
-        } //end if
-    }//end for    
-    }//end for
 
-    printListOfDoubles (listOfDoubles, COUNT);
+    sortVisualize (listOfDoubles, COUNT);
     
     gfx_open(xSize, ySize, "BarChart");
     gfx_color(255,0,0);
@@ -72,22 +51,6 @@ int main ()
     return 0;   
 
 }//end main
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
